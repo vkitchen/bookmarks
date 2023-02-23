4b57a9059ff0b82844cc25426b65308dd1e3b452
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2023-02-23 20:27:00 +1300

    Destroy bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -35,6 +35,13 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def bookmark_params
     params.require(:bookmark).permit(:url, :title)
